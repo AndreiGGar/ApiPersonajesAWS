d41a267646bf5a8515476c1b9c60367eb94f6234
andreiggar <andreiggar@gmail.com>
2023-05-24 13:12:02 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Context/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Personaje> Personajes { get; set; }
+    }
+
+}
